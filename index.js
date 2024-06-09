be508d19d874f35f988ea159bb19981267d32d5b
BuhleMtshali <mtshalizanobuhle7@gmail.com>
2024-06-09 23:07:16 +0200

    updated the index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+let inputElement = document.querySelector("#input");
+let weightElement = document.querySelector("#weight");
+
+function convertNow(event) {
+  event.preventDefault();
+  let newWeight = inputElement.value * 2.205;
+  weightElement.textContent = " " + Math.floor(newWeight) + "kg";
+}
+
+let submitButton = document.querySelector("#submit");
+submitButton.addEventListener("click", convertNow);
